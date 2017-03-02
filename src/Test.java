a322a28332f85188f7ff68a6338e90e6482b5b8a
Alex New <newaj01@students.ipfw.edu>
2017-03-02 11:21:46 -0500

    Testing push from Intellij

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,8 @@
-/**
- * Created by Alex New on 3/2/2017.
- */
+
 public class Test {
     public static void main(String[] args){
         System.out.println("Test");
+        System.out.println("Updating GitHub");
     }
 
 }
