3e2c115767506fb4edde8a86c4446a40b98342f0
Benjamin <schmbl04@students.ipfw>
2017-03-27 12:03:11 -0400

    Added ScoreBoardGUI and model

---
+++ src/model/ScoreBoard.java
@@ -0,0 +1,7 @@
+package model;
+
+/**
+ * Created by Benjamin on 3/27/2017.
+ */
+public class ScoreBoard {
+}
---
+++ src/view/ScoreBoardGUI.java
@@ -0,0 +1,7 @@
+package view;
+
+/**
+ * Created by Benjamin on 3/27/2017.
+ */
+public class ScoreBoardGUI {
+}
