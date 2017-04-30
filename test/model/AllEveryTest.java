e49144ea34b45b1d9a9e5fba4d9d018d923892c1
levin <kimjh01@students.ipfw.edu>
2017-04-29 20:33:05 -0400

    added Alleverytest

---
+++ test/model/AllEveryTest.java
@@ -0,0 +1,12 @@
+package model;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by levin on 4/29/17.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({getSurroundingValuesValid.class, getSurroundingValuesInvalid.class,performMoveValid.class, performMoveInvalid.class,})
+public class AllEveryTest {
+}
--- test/model/performMoveInvalid.java
+++ test/model/performMoveInvalid.java
@@ -1,19 +1,34 @@
 package model;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class performMoveInvalid {
 
-    @BeforeClass
-    public static void onlyOnce(){
-        TileQueue testQueue = new TileQueue(false);
-        Tile[][] board = new Tile[9][9];
-        int row, column;
+    private Tile[][] board;
+    private Game game;
+    private int row, column;
+    private int expected, result;
+
+    @Before
+    public void setup(){
+        game = UntimedGame.getInstance();
+        board = game.getBoard();
     }
 
     @Test
-    public void getSurroundingValues() {
+    public void testPerformMoveFor_A() {
+        row = 0;
+        column = 0;
+
+        board[column][row - 1].setNumber(1);
+        board[column + 1][row + 1].setNumber(1);
+        board[column + 1][row].setNumber(1);
+
+
+        Assert.assertEquals(expected, result);
     }
 
 }
